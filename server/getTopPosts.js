c485727657c6e2f5dee3b48a0f2566cbbececf72
Po Wei Tsao <poweitsao@gmail.com>
2020-05-03 04:32:07 -0400

    completed text extraction. Posts are now stored in JSON files.

--- server/getTopPosts.js
+++ server/getTopPosts.js
@@ -1,11 +1,12 @@
 // const snoowrap = require('snoowrap');
 const fs = require('fs')
 const r = require("./app.js")
+const contentTest = require("./contentTest.json")
 
 // r.getHot().map(post => post.title).then(console.log);
 
 async function getTopPosts(subreddit, time, limit) {
-    const content = await r.getTop(subreddit, { time: time, limit: limit })
+    const content = await r.getTop(subreddit, { time: time })
     const filteredContent = await extractContent(content)
     return filteredContent
 }
@@ -13,27 +14,68 @@ async function getTopPosts(subreddit, time, limit) {
 
 const extractContent = (input) => {
     var clonedInput = JSON.parse(JSON.stringify(input))
-    // var jsonContent = JSON.stringify(input)
-    var posts = []
+    var keys = []
+    var key = ""
 
+    var splitPosts = {}
+    console.log(clonedInput.length)
     for (i = 0; i < clonedInput.length; i++) {
+        console.log(i)
+        console.log(clonedInput.length)
+
         var post = clonedInput[i]
-        var content = {}
-        content["selftext"] = post["selftext"]
-        content["title"] = post["title"]
-        content["id"] = post["id"]
-        // console.log(content)
-        posts.push(content)
+
+        var body = JSON.stringify(post["selftext"])
+        var postSegments = []
+        key = "post" + i
+
+        keys.push(key)
+
+        if (body.length > 5000) {
+            var remainingLength = body.length
+            var currentStart = 0
+            var currentEnd = 4999
+
+            while (remainingLength > 5000) {
+                if (body[currentEnd - 1] != ".") {
+                    for (j = currentEnd - 1; j > currentStart; j--) {
+                        if (body[j] != ".") {
+                            currentEnd = j + 1
+                            console.log("break")
+                            break;
+                        }
+                    }
+                }
+                console.log("while")
+
+                postSegments.push(body.substring(currentStart, currentEnd))
+                currentStart = currentEnd
+                currentEnd = currentStart + 4999
+                remainingLength = body.length - currentStart
+
+            }
+            if (remainingLength > 0) {
+                postSegments.push(body.substring(currentStart, body.length))
+                console.log("push")
+                console.log(i)
+            }
+
+        } else if (body.length < 5000) {
+
+            postSegments.push(body)
+        }
+
+        splitPosts[key] = postSegments
     }
-    return posts
+
+    splitPosts["keys"] = keys
+    console.log(splitPosts["keys"])
+    return splitPosts
 
 }
-async function run() {
 
-    var result = await getTopPosts("tifu", "day", 2)
-    var stringifiedResult = JSON.stringify(result)
-    // console.log(stringifiedResult[0]["title"])
-    fs.writeFile("response1.json", stringifiedResult, function (err) {
+const writeFile = (input, filename) => {
+    fs.writeFile(filename, input, function (err) {
         if (err) {
             console.log("An error occured while writing JSON Object to File.");
             return console.log(err);
@@ -41,7 +83,15 @@ async function run() {
 
         console.log("JSON file has been saved.");
     })
-
 }
 
-run()
+getTopPosts("tifu", "day", 11).then((result) => {
+    var stringifiedResult = JSON.stringify(result)
+    writeFile(stringifiedResult, "response2.json")
+})
+
+
+
+
+
+
